f5e13def2e174f0d4ca1cc73c3f6c5be3953d720
Nidiv Kayastha <nidiv04@gmail.com>
2024-07-22 16:54:56 +0545

    Created foods and a snake

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const gameBoard = document.querySelector("#gameBoard");
-const ctx = gameBoard.getcontext("2d");
-const scoreText = document.querySelector("scoreText");
+const ctx = gameBoard.getContext("2d");
+const scoreText = document.querySelector("#scoreText");
 const resetBtn = document.querySelector("resetBtn");
 const gameWidth = gameBoard.width;
 const gameHeight = gameBoard.height;
@@ -24,15 +24,57 @@ let snake = [
 ];
 
 window.addEventListener("keydown", changeDirection);
-resetBtn.addEventListener("click", resetGame);
+// resetBtn.addEventListener("click", resetGame);
 
 gameStart();
-function gameStart() {}
-function nextTick() {}
-function clearBoard() {}
-function createfood() {}
-function drawFood() {}
-function drawSnake() {}
+function gameStart() {
+  running = true;
+  scoreText.textContent = score;
+  createFood();
+  drawFood();
+  nextTick();
+}
+function nextTick() {
+  if (running) {
+    setTimeout(() => {
+      clearBoard();
+      drawFood();
+      moveSnake();
+      drawSnake();
+      checkGameOver();
+      nextTick();
+    }, 75);
+  } else {
+    displayGameOver();
+  }
+}
+function clearBoard() {
+  ctx.fillStyle = boardBackground;
+  ctx.fillRect(0, 0, gameWidth, gameHeight);
+}
+function createFood() {
+  function randomFood(min, max) {
+    const randNum =
+      Math.round((Math.random() * (max - min) + min) / unitSize) * unitSize;
+    return randNum;
+  }
+  foodX = randomFood(0, gameWidth - unitSize);
+  foodY = randomFood(0, gameWidth - unitSize);
+}
+function drawFood() {
+  ctx.fillStyle = foodColor;
+  ctx.fillRect(foodX, foodY, unitSize, unitSize);
+}
+function moveSnake() {}
+function drawSnake() {
+  ctx.fillStyle = snakeColor;
+  ctx.strokeStyle = snakeBorder;
+  snake.forEach((snakePart) => {
+    ctx.fillRect(snakePart.x, snakePart.y, unitSize, unitSize);
+    ctx.strokeRect(snakePart.x, snakePart.y, unitSize, unitSize);
+  });
+}
 function changeDirection() {}
 function checkGameOver() {}
+function displayGameOver() {}
 function resetGame() {}
